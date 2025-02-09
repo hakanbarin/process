c6942bed1cc589e2eb394c0548a0ed3bc6c3f256
hakanbarin <barinhakan@hotmail.com>
2025-02-09 21:15:21 +0300

    first commit

---
+++ processdeneme.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/wait.h>
+
+int main(){
+
+
+    int id = fork();
+    printf("Current %d, Parent %d\n",getpid(), getppid());
+}
+
+
+
+/*
+int main(){
+    int id = fork();
+    int n;
+    if(id == 0){
+        n = 1;
+    }
+    else{
+        n = 6;
+    }
+    if(id != 0){
+
+        wait(NULL);
+    }
+    int i;
+    for(i = n; i < n + 5; i++){
+        printf("%d ", i);
+        fflush(stdout);
+    }
+
+    if(id != 0){
+
+        printf("\n");
+    }
+
+}
+    */
